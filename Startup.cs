8d17a895cd2f997d32c6c4a3f86e5eb5e431b4c5
Viliuskerutis <32612736+Viliuskerutis@users.noreply.github.com>
2022-04-27 15:59:25 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,25 @@
+﻿using KronoBattleship.DESIGN_PATTERNS.Factory;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KronoBattleship.Startup))]
+namespace KronoBattleship
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            /**FACTORY IS WORKING**/
+            /** 
+            string a = Factory.Get(1).ProductName;
+             string b = Factory.Get(2).ProductName;
+
+             var aa = Factory.Get(1).Product;
+             var bb = Factory.Get(2).Product;
+           **/
+
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
